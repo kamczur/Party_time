58e26f9aca696bed89964ce1e4fb3183a0f39d3c
kamczur <k.czuryszkiewicz@protonmailcom>
2022-06-14 22:31:14 +0200

    changes

--- imprezy/views.py
+++ imprezy/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
 
 from .forms import NewUserForm, AddPartyForm, GiftForm, GuestForm
@@ -8,10 +9,18 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 
+
 def homepage(request):
+    """Load the homepage
+    :return: Homepage html
+    """
     return render(request=request, template_name='homepage.html')
 
+
 def register_request(request):
+    """Register the new app user
+    :return: Register Form
+    """
     if request.method == "POST":
         form = NewUserForm(request.POST)
         if form.is_valid():
@@ -25,8 +34,10 @@ def register_request(request):
     return render (request=request, template_name="register.html", context={"register_form":form})
 
 
-
 def login_request(request):
+    """Login user or inform about wrong credentials
+    :return: Login Form
+    """
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
@@ -44,17 +55,25 @@ def login_request(request):
     form = AuthenticationForm()
     return render(request=request, template_name="login.html", context={"login_form":form})
 
+
 def logout_request(request):
+    """Logout user
+    :return: Homepage
+    """
     logout(request)
     messages.info(request, "Wylogowano się poprawnie.")
     return redirect("homepage")
 
+
 class AddParty(LoginRequiredMixin, View):
+    """Allows the logged user to add the party
+    :return: Party Form"""
+
     login_url = "/login"
 
     def get(self, request):
         form = AddPartyForm()
-        return render(request, 'addParty.html', {'form':form})
+        return render(request, 'addParty.html', {'form': form})
 
     def post(self, request):
         form = AddPartyForm(request.POST)
@@ -71,13 +90,20 @@ class AddParty(LoginRequiredMixin, View):
 
 
 class PartiesListView(LoginRequiredMixin, View):
+    """displays the list of all parties stored in DB
+    :return: Parties html"""
     login_url = "/login"
+
     def get(self, request):
         parties = Party.objects.all()
         return render(request, "parties.html", context={"parties": parties})
 
 
 class AddGiftView(View):
+    """allows the logged user to add a desirable gift
+    :return: Gifts Form
+    """
+
     def get(self, request):
         form = GiftForm()
         return render(request, 'gifts.html', {'form':form})
@@ -94,35 +120,46 @@ class AddGiftView(View):
 
 
 class GiftsListView(View):
+    """displays the list of all gifs stored in DB
+    :return: Gifts list Html
+    """
     def get(self, request):
-        gifts = Gift.objects.all()
+        gifts = Gift.objects.all().order_by('gift_name')
         return render(request, "giftsList.html", {"gifts": gifts})
 
 
 class ReserveGiftView(View):
+    """allows the user to reserve a gift to buy
+    :return: Gifts List html"""
+
     def get(self, request, gift_id):
         gift = Gift.objects.get(id=gift_id)
-        return render(request, "giftsList.html", {"gift":gift})
+        return render(request, "giftsList.html", {"gift": gift})
 
     def post(self, request, gift_id):
         gifts = Gift.objects.all()
         gift = Gift.objects.get(id=gift_id)
         comment = request.POST.get("comment")
-        p = GiftReservation.objects.create(gift=gift, comment=comment)
+        p = GiftReservation.objects.create(gift=gift, comment=comment, availability=False)
         for gift in gifts:
             gift.reserved = GiftReservation.availability == False
             p.save()
-        return redirect("gifts-list")
-
+            return redirect('gifts-list')
 
 
 class DeleteGiftView(View):
+    """delete gift
+    :return: Gifts List Html"""
     def get(self, request, gift_id):
         gift = Gift.objects.get(id=gift_id)
         gift.delete()
         return redirect("gifts-list")
 
+
 class DeletePartyView(View):
+    """delete party
+    :return: Party List Html
+    """
     def get(self, request, party_id):
         party = Party.objects.get(id=party_id)
         party.delete()
@@ -130,6 +167,8 @@ class DeletePartyView(View):
 
 
 class EditPartyView(View):
+    """edit party
+    :return: Party Form"""
     def get(self, request, party_id):
         party = Party.objects.get(id=party_id)
         data = {'party_name': party.party_name, 'party_date': party.party_date, 'party_time': party.party_time, 'description':party.description}
@@ -155,13 +194,17 @@ class EditPartyView(View):
 
 
 class PartyDetailsView(View):
+    """displays details of party
+    :returns: Details Party Html"""
     def get(self, request, party_id):
         party = Party.objects.get(id=party_id)
         return render(request, "detailsParty.html", {'party':party})
 
 
 class GuestsView(View):
-
+    """alows user to sign up to a party
+    :returns: Party Form
+    """
     def get(self, request, party_id):
         form = GuestForm()
         party = Party.objects.get(id=party_id)
@@ -187,6 +230,9 @@ class GuestsView(View):
 
 
 class LastPageView(View):
+    """displays last page
+    :return: Last Page Html
+    """
     def get(self, request, party_id):
         party = Party.objects.get(id=party_id)
         return render(request, "lastPage.html", {'party':party})
